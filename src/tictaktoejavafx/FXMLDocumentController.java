a53e55c25d8610fa8515947064ffae6dfb345694
Mohamed Mounir <dev.mohamd.arfa@gmail.com>
2022-12-03 17:38:21 +0200

    repase project files

--- src/tictaktoejavafx/FXMLDocumentController.java
+++ src/tictaktoejavafx/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tictagg;
+package tictaktoejavafx;
 
 import java.net.URL;
 import java.util.ResourceBundle;
--- src/tictaktoejavafx/TicTakToeJavaFX.java
+++ src/tictaktoejavafx/TicTakToeJavaFX.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tictagg;
+package tictaktoejavafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
  *
  * @author Elsawah
  */
-public class TicTagg extends Application {
+public class TicTakToeJavaFX extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
