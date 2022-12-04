5c4263f4a49e328520f1e89f57d9efa7b219bcf6
unknown <emankhaledsha@gmail.com>
2022-12-04 22:33:59 +0200

    database

--- server/src/server/Server.java
+++ server/src/server/Server.java
@@ -1,11 +1,19 @@
 package server;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.derby.jdbc.ClientDriver;
 import server.controller.ServerHomeController;
+import org.apache.derby.jdbc.ClientDriver;
+
 
 public class Server extends Application {
     
@@ -18,7 +26,13 @@ public class Server extends Application {
     }
     
     public static void main(String[] args) {
-        launch(args);
+       try {
+            launch(args);
+            DriverManager.deregisterDriver(new ClientDriver());
+           Connection con = DriverManager.getConnection( "jdbc:derby://localhost:1527/userdatabase","root" ,"root");
+        } catch (SQLException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
