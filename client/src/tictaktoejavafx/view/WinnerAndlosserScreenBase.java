141e1b00d8530cdfe600e0bd87d304813f63b753
Mohamed Mounir <dev.mohamd.arfa@gmail.com>
2022-12-07 17:50:22 +0200

    video Player fixes

--- client/src/tictaktoejavafx/view/WinnerAndlosserScreenBase.java
+++ client/src/tictaktoejavafx/view/WinnerAndlosserScreenBase.java
@@ -1,13 +1,8 @@
 package tictaktoejavafx.view;
 
-import java.nio.file.Paths;
 import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-import tictaktoejavafx.utils.PlayerName;
 
 public abstract class WinnerAndlosserScreenBase extends AnchorPane {
 
@@ -28,25 +23,24 @@ public abstract class WinnerAndlosserScreenBase extends AnchorPane {
         setPrefHeight(512.0);
         setPrefWidth(845.0);
 
-        label.setLayoutX(342.0);
-        label.setLayoutY(38.0);
+        label.setLayoutX(312.0);
+        label.setLayoutY(22.0);
         label.setPrefHeight(43.0);
         label.setPrefWidth(92.0);
         label.setText("Winner is ");
 
-     
-
-        user_win.setLayoutX(423.0);
-        user_win.setLayoutY(49.0);
-        user_win.setText(PlayerName.getPlayerName());
+        user_win.setLayoutX(394.0);
+        user_win.setLayoutY(35.0);
+        user_win.setText("user_name");
 
         mediaView.setFitHeight(400.0);
         mediaView.setFitWidth(400.0);
-        mediaView.setLayoutX(295.0);
-        mediaView.setLayoutY(143.0);
+        mediaView.setLayoutX(204.0);
+        mediaView.setLayoutY(88.0);
 
         getChildren().add(label);
         getChildren().add(user_win);
         getChildren().add(mediaView);
+
     }
 }
