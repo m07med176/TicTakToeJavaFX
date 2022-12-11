c2af48f73c80fa6aa27b39f4472fd8b2f97ebade
Hussien <sonictwester@gmail.com>
2022-12-11 11:23:16 +0200

    added DB functions (first three)

--- server/src/server/data/db/DatabaseAccessLayer.java
+++ server/src/server/data/db/DatabaseAccessLayer.java
@@ -1,6 +1,7 @@
 package server.data.db;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -17,29 +18,66 @@ public abstract class DatabaseAccessLayer implements DatabaseCRUD {
     public static String tableName = "player";
     static Player player;
     static int rs2;
+    PreparedStatement statement;
+    
 
     public DatabaseAccessLayer() throws SQLException {
         con = DataBaseConnection.getConnection();
     }
 
     @Override
-    public ArrayList<Player> getOnlinePlayers() {
-        // TODO get online users from database
+     public ArrayList<Player> getOnlinePlayers() {
+         ArrayList<Player> players=new ArrayList();
+         try {
+             // TODO get online users from database
+             statement=con.prepareStatement("select username from player where status=?");
+             statement.setBoolean(1, true);
+             rs=statement.executeQuery();
+             while(rs.next()){
+                 players.add(new Player(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getInt(4),rs.getBoolean(5)));
+         
+             }
+             
+         } catch (SQLException ex) {
+             Logger.getLogger(DatabaseAccessLayer.class.getName()).log(Level.SEVERE, null, ex);
+         }
+         return players;
+     }
 
-        return new ArrayList();
-    }
 
-    @Override
-    public int getCountPlayers() {
-        // TODO get numbers of users from database
-        return 0;
-    }
+     @Override
+     public int getCountPlayers() {
+         int countPlayer=0;
+         try {
+             // TODO get numbers of users from database
+             statement=con.prepareStatement("select COUNT(*) from player");
+             rs=statement.executeQuery();
+             countPlayer=rs.getInt(1);
+         } catch (SQLException ex) {
+             Logger.getLogger(DatabaseAccessLayer.class.getName()).log(Level.SEVERE, null, ex);
+         }
+         return countPlayer;
+     }
+     @Override
+     public String addPlayer(Player user) {
+         String state=user.getUsername();
+         try {
+             // TODO add new row of player in database
+             statement=con.prepareStatement("insert into player(id,email,username,password,status) values(?,?,?,?,?)");
+             statement.setInt(1, user.getId());
+             statement.setString(2, user.getEmail());
+             statement.setString(3, user.getUsername());
+             statement.setInt(4, user.getPassword());
+             statement.setBoolean(5, user.isStatus());
+             rs2=statement.executeUpdate();
+             state=state+" data has been added";
+             //return "";
+         } catch (SQLException ex) {
+             Logger.getLogger(DatabaseAccessLayer.class.getName()).log(Level.SEVERE, null, ex);
+         }
+         return state;
+     }
 
-    @Override
-    public String addPlayer(Player user) {
-        // TODO add new row of Player in database
-        return "";
-    }
 
     @Override
     public boolean updatePlayerStatus(boolean status, int id) {
