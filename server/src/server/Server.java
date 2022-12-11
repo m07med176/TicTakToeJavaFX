5d3bb1f44fc519976d0f5413839eda314bd47efc
AyaMohamedAhmed <ayamohamed3420@gmail.com>
2022-12-11 18:19:22 +0200

    Confirm Dialog

--- server/src/server/Server.java
+++ server/src/server/Server.java
@@ -1,7 +1,9 @@
 package server;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import server.utils.AlertAction;
 import server.utils.Config;
+import server.utils.ConfirmMessage;
 import server.utils.Navigator;
 
 
@@ -11,12 +13,27 @@ public class Server extends Application {
     public void start(Stage stage) throws Exception {
         CSS = this.getClass().getResource(Config.CSS).toExternalForm();
         Navigator.navigate(Navigator.HOME, stage);
+        ConfirmMessage con=new ConfirmMessage();
+        con.display("you should enter ",new AlertAction(){
+            @Override
+            public void sendOk() {
+                System.out.println("ok");    
+            }
+
+            @Override
+            public void sendCancel() {
+                System.out.println("cancel");
+            }
+            
+        });
+       
         // TODO set fixed size in all pages 
         // TODO make size resizable
     }
     
     public static void main(String[] args) {
          launch(args);
+         
     }
     
 }
