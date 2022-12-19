4e4682076aa943f64e1151d64dd2b3175f8a5ed2
Mohamed Mounir <dev.mohamd.arfa@gmail.com>
2022-12-19 15:00:41 +0200

    fix imports

--- client/src/tictaktoejavafx/Client.java
+++ client/src/tictaktoejavafx/Client.java
@@ -1,13 +1,11 @@
 package tictaktoejavafx;
 
 import java.io.DataInputStream;
-import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
 import tictaktoejavafx.utils.PathManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import tictaktoejavafx.data.server.ServerConnection;
 import tictaktoejavafx.utils.Config;
 import tictaktoejavafx.utils.Navigator;
 
@@ -23,8 +21,6 @@ public class Client extends Application {
         CSS = this.getClass().getResource(Config.CSS).toExternalForm();
         PathManager.getPath();
         Navigator.navigate(Navigator.SPLACH, stage);
-       // new HistoryJson(new PlayerModel());
-        
         // TODO set fixed size in all pages 
         // TODO make size resizable
     }
--- client/src/tictaktoejavafx/controller/GameBoardControllerOnline.java
+++ client/src/tictaktoejavafx/controller/GameBoardControllerOnline.java
@@ -1,8 +1,8 @@
 package tictaktoejavafx.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
